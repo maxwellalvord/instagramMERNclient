89a22ed54c67285aa7cd4c5ae0a6188a02b226d9
Maxwell Alvord <maxwellalvord@gmail.com>
2023-02-13 12:44:47 -0800

    update: start building the nav bar for mobile and desktop

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -1,6 +1,76 @@
+import { useState } from "react"
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormCOntrol,
+  useThem,
+  useMediaQuery
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close
+} from "@mui/icons-material";
+import { useDispatch, userDispatcher, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+
 const Navbar = () => {
+  const [isMobileMenuToggled, setisMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("min-width: 1000px");
+
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
+  const fullName = `${user.firstName} ${user.lastName}`
+
   return (
-    <div>navbar</div>
+    <FlexBetween padding= "1rem 6%" backgroundCOlor={alt}>
+      <FlexBetween gap="1.75rem">
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem, 2rem, 2.25rem)"
+          color="primary"
+          onClick={() => navigate("/home")}
+          sx={{
+            "&hover": {
+              color: PrimaryLight,
+              cursor: "pointer",
+            },
+          }}
+        >
+          The Society
+        </Typography>
+        {isNonMobileScreens && (
+          <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
+            <InputBase placeholder="Search the Society..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        )}
+      </FlexBetween>
+
+      {/* DESKTOP NAV */}
+      
+    </FlexBetween>
   )
 }
 
